fc986c0f9a611597638ac49935d11011b786a41f
yyd <yyddy1993@163.com>
2014-10-13 11:14:02 +0800

    nine

---
+++ H12.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+    float x=2.5;
+	float y=2.5;
+	float z=2.5;
+    printf("These values are:\n");
+    printf("x=%f\n",x);
+	printf("y=%f\n",y);
+	printf("z=%f\n",z);
+}
